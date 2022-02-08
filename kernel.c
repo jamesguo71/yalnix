d2793912c8a2eb0fd840566644424fdc5c4ae5c7
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-08 15:17:40 -0500

    i think we need to subtract the max size of the page table when calculating page numbers for region 1 bc they will produce indexes 128-255, but our page table indexes are 0-127

--- kernel.c
+++ kernel.c
@@ -343,7 +343,7 @@ void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uctxt) {
     //       above the kernel stack, maybe we should just call our FindFreeFrame function---it may
     //       return a frame within region 0 or it may return a frame in region 1 if we have enough
     //       physical memory for it. Does that make sense?
-    int user_stack_page_num  = ((int ) idlePCB->uctxt->sp) >> PAGESHIFT;
+    int user_stack_page_num  = (((int ) idlePCB->uctxt->sp) >> PAGESHIFT) - MAX_PT_LEN;
     int user_stack_frame_num = FindFreeFrame();
     if (user_stack_frame_num == ERROR) {
         TracePrintf(1, "Unable to find free frame for DoIdle userstack!\n");
@@ -351,7 +351,7 @@ void KernelStart(char **cmd_args, unsigned int pmem_size, UserContext *uctxt) {
     }
 
     SetPTE(user_pt,                   // page table pointer
-           user_stack_page_num - MAX_PT_LEN,           // page number
+           user_stack_page_num,           // page number
            1,                         // valid bit
            PROT_READ | PROT_WRITE,    // page protection bits
            user_stack_frame_num);          // frame number
