a983eeac19d6d4c7a5d5903d265bd8266d710b72
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-11 15:51:44 -0500

    testing out syscallbrk and syscallgetpid

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -459,11 +459,8 @@ KernelContext *MyKCS(KernelContext *kc_in, void *curr_pcb_p, void *next_pcb_p) {
  * \desc  A dummy userland process that the kernel runs when there are no other processes.
  */
 static void DoIdle(void) {
-    char *string = (char *) calloc(30000, sizeof(char));
-    strcpy(string, "DoIdle");
-    int pid = SyscallGetPid();
     while(1) {
-        TracePrintf(1,"%s pid: %d\n", pid);
+        TracePrintf(1,"DoIdle\n", pid);
         Pause();
     }
 }
