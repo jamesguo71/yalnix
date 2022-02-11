adeb1776d262645736927cdba72e2bb292495aa1
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-11 15:32:20 -0500

    accidently defined it as FRAME_H bc i copied code lol

--- kernel/pte.h
+++ kernel/pte.h
@@ -1,7 +1,7 @@
-#ifndef __FRAME_H
-#define __FRAME_H
+#ifndef __PTE_H
+#define __PTE_H
 #include <hardware.h>
 
 int PTEClear(pte_t *pt, int page_num);
 int PTESet(pte_t *pt, int page_num, int prot, int pfn);
-#endif // __FRAME_H
+#endif // __PTE_H
