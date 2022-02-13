f33eb525db8ed74da5b4ccf07b891c906493e4bc
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-13 18:10:05 -0500

    changing logic for calculating stack address in kccopy

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -501,15 +501,13 @@ KernelContext *KCCopy(KernelContext *_kctxt, void *_new_pcb_p, void *_not_used) 
     //    stack to the temporary stack---remember, the temporary pages are mapped to
     //    the new process' stack frames, so even though these pages are temporary for
     //    the current process the new process still has a reference to the stack frames.
-    void *kernel_stack_start_addr = (void *) (kernel_stack_start_page_num << PAGESHIFT);
-    void *kernel_stack_temp_addr  = (void *) (kernel_stack_temp_page_num  << PAGESHIFT);
-    // void *kernel_stack_start_addr = (void *) (KERNEL_STACK_LIMIT - KERNEL_STACK_MAXSIZE);
-    // void *kernel_stack_temp_addr = kernel_stack_start_addr - KERNEL_STACK_MAXSIZE;
+    // void *kernel_stack_start_addr = (void *) (kernel_stack_start_page_num << PAGESHIFT);
+    // void *kernel_stack_temp_addr  = (void *) (kernel_stack_temp_page_num  << PAGESHIFT);
+    void *kernel_stack_start_addr = (void *) (KERNEL_STACK_LIMIT - KERNEL_STACK_MAXSIZE);
+    void *kernel_stack_temp_addr = kernel_stack_start_addr - KERNEL_STACK_MAXSIZE;
     for (int i = 0; i < KERNEL_NUMBER_STACK_FRAMES; i++) {
         TracePrintf(1, "[MyKCCopy] stack_start: %p\ttemp_start: %p\n",
                        kernel_stack_start_addr, kernel_stack_temp_addr);
-        TracePrintf(1, "[MyKCCopy] stack_page: %d\ttemp_page: %d\n",
-                       DOWN_TO_PAGE(kernel_stack_start_addr), DOWN_TO_PAGE(kernel_stack_temp_addr));
         memcpy(kernel_stack_temp_addr, kernel_stack_start_addr, PAGESIZE);
         kernel_stack_start_addr += PAGESIZE;
         kernel_stack_temp_addr += PAGESIZE;
