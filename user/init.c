d95d4b3ed53ca47b67d48c620e9fd5e86397bcfe
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-15 13:45:01 -0500

    adding call to getpid

--- user/init.c
+++ user/init.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char **argv) {
     while(1) {
-        TracePrintf(1, "Init delaying for 2 clock cycles\n");
+        int pid = GetPid();
+        TracePrintf(1, "Init pid: %d. Delaying for 2 clock cycles\n", pid);
         Delay(2);
     }
     return 0;
