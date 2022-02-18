0d26ca23ff6417c47bcd5da14c8fdec3d3ae7a00
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-17 19:40:55 -0500

    for now im just going to leave pcb struct in scheduler...

--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -23,8 +23,29 @@
 #define SCHEDULER_NUM_LISTS        17
 
 
-extern typedef struct pcb_t;
 typedef struct scheduler scheduler_t;
+
+// TODO: Add a char *name field for debugging/readability?
+typedef struct pcb {
+    int  pid;
+    int  clock_ticks;
+    int  exit_status;       // for saving the process's exit status, See Page 32
+    int  exited;            // if the process has exited?  
+    int *held_locks;        // locks held by the current process, used by sync syscalls
+
+    struct pcb *parent;     // For keeping track of parent process
+    struct pcb *children;   // For keeping track of children processes
+
+    KernelContext *kctxt;   // Needed for KernelCopy? See Page 45 
+    UserContext   *uctxt;   // Defined in `hardware.h`    
+
+    pte_t *ks;
+    pte_t *pt;              // Defined in `hardware.h`
+
+    void *brk;
+    void *text_end;
+    void *data_end;
+} pcb_t;
 
 
 /*!
