cec7e56361d4526137e688f79490b989715d196b
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-17 19:54:33 -0500

    creating a process module. starting with just the struct for now

---
+++ kernel/process.h
@@ -0,0 +1,28 @@
+#ifndef __PROCESS_H
+#define __PROCESS_H
+#include <hardware.h>
+
+
+// TODO: Add a char *name field for debugging/readability?
+typedef struct pcb {
+    int  pid;
+    int  clock_ticks;
+    int  exit_status;       // for saving the process's exit status, See Page 32
+    int  exited;            // if the process has exited?  
+    int *held_locks;        // locks held by the current process, used by sync syscalls
+
+    struct pcb *parent;     // For keeping track of parent process
+    struct pcb *children;   // For keeping track of children processes
+
+    KernelContext *kctxt;   // Needed for KernelCopy? See Page 45 
+    UserContext   *uctxt;   // Defined in `hardware.h`    
+
+    pte_t *ks;
+    pte_t *pt;              // Defined in `hardware.h`
+
+    void *brk;
+    void *text_end;
+    void *data_end;
+} pcb_t;
+
+#endif // __PROCESS_H
