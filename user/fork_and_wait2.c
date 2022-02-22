4080cd603ac7563a51b88ee640a1b6156a584008
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-22 17:49:02 -0500

    adding while loop for parent so it doesnt halt the system by exiting

--- user/fork_and_wait2.c
+++ user/fork_and_wait2.c
@@ -13,6 +13,9 @@ int main() {
         TracePrintf(1, "[fork_and_wait] Parent waiting on child: %d to finish...\n", pid);
         int status;
         pid = Wait(&status);
+        while(1) {
+            TracePrintf(1, "[fork_and_wait] Parent received child: %d exit status: %d\n", pid, status);
+        }
     } 
 
     // child process
