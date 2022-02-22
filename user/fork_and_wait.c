7953f3f5f2820e133efa897318a420b90f6f4d7f
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-22 14:42:58 -0500

    added multiple child processes and parent exits at the end now too

--- user/fork_and_wait.c
+++ user/fork_and_wait.c
@@ -1,16 +1,23 @@
 #include "yuser.h"
 
 int main() {
-    int pid = Fork();
-    if (pid) {
-        TracePrintf(1, "[fork_and_wait] Parent waiting on child: %d\n", pid);
-        int status;
-        Wait(&status);
-        while (1) {
-            TracePrintf(1, "[fork_and_wait] Parent received child's exit status: %d\n", status);
-            Pause();
+    int num_children = 5;
+    for (int i = 0; i < num_children; i++) {
+        int pid = Fork();
+        if (pid) {
+            TracePrintf(1, "[fork_and_wait] Parent forked child process: %d\n", pid);
+        } else {
+            pid = GetPid();
+            TracePrintf(1, "[fork_and_wait] Child %d exiting...\n", pid);
+            return 0;
         }
     }
-    TracePrintf(1, "[fork_and_wait] Child fork return value: %d \n", pid);
+
+    for (int i = 0; i < num_children; i++) {
+        TracePrintf(1, "[fork_and_wait] Parent waiting on any child to finish...\n");
+        int status;
+        int pid = Wait(&status);
+        TracePrintf(1, "[fork_and_wait] Parent received child: %d exit status: %d\n", pid, status);
+    }
     return 0;
 }
