30efd2a419f158b3def45c82c5697162abb6c09d
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-23 21:03:55 -0500

    added include for process for pcb struct. how did this ever compile before?

--- kernel/load_program.h
+++ kernel/load_program.h
@@ -1,6 +1,9 @@
 #ifndef YALNIX_FRAMEWORK_LOAD_PROGRAM_H
 #define YALNIX_FRAMEWORK_LOAD_PROGRAM_H
 
+#include "process.h"
+
+
 int LoadProgram(char *name, char *args[], pcb_t *proc);
 
 #endif // YALNIX_FRAMEWORK_LOAD_PROGRAM_H
