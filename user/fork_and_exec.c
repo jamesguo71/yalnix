c540ce3de400de28fb2f24d18047104037d86ce6
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-23 21:13:33 -0500

    adding test file for exec

---
+++ user/fork_and_exec.c
@@ -0,0 +1,22 @@
+#include "yuser.h"
+
+int main() {
+    int pid = Fork();
+    if (pid) {
+        while (1) {
+            TracePrintf(1, "[fork_and_exec] Parent forked child: %d\n", pid);
+            Pause();
+        }
+    } else {
+        char *program   = "./user/chpt3_test.c";
+        char *argvec[1];
+        argvec[0]       = "./user/chpt3_test.c";
+        TracePrintf(1, "[fork_and_exec] Child about to exec: %s \n", program);
+        Exec(program, argvec);
+        while (1) {
+            TracePrintf(1, "[fork_and_exec] Child failed to exec: %s\n");
+            Pause();
+        }
+    }
+    return 0;
+}
