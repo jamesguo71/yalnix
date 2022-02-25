5c9c7ff7def9569b21bf26271e2832ad4837b71f
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-24 19:43:36 -0500

    adding test program for trap math

--- user/trap_math.c
+++ user/trap_math.c
@@ -1,7 +1,14 @@
 #include <yuser.h>
 
-int main(int argc, char **argv) {
+int main() {
+    int pid = Fork();
+    if (pid) {
+        while (1) {
+            TracePrintf(1, "[trap_math] Parent forked child: %d\n", pid);
+            Delay(2);
+        }
+    }
     int answer = 42 / 0;
-    TracePrintf(1, "[trap_math] The answer is: %d\n", answer);
+    TracePrintf(1, "[trap_math] Child divided by zero answer is: %d\n", answer);
     return 0;
 }
