5a09df9305dc9470b34ae1936983ccbd70c691a4
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-24 20:35:24 -0500

    adding trap memory test

---
+++ user/trap_memory.c
@@ -0,0 +1,13 @@
+#include <yuser.h>
+
+void recurse(int depth) {
+    TracePrintf(1, "[trap_memory] Recurse depth: %d\n", depth);
+    int array[1000];
+    recurse(depth--);
+}
+
+int main() {
+    int depth = 10;
+    recurse(depth);
+    return 0;
+}
