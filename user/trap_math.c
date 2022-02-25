1d6e75febc478fd799b3f68272867346dc166f21
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-24 19:46:58 -0500

    adding test program for trap math

--- user/trap_math.c
+++ user/trap_math.c
@@ -1,13 +1,20 @@
 #include <yuser.h>
 
 int main() {
+    // parent process
     int pid = Fork();
     if (pid) {
-        while (1) {
-            TracePrintf(1, "[trap_math] Parent forked child: %d\n", pid);
+        TracePrintf(1, "[fork_and_wait] Parent waiting on child: %d to finish...\n", pid);
+        int status;
+        pid = Wait(&status);
+        while(1) {
+            TracePrintf(1, "[fork_and_wait] Parent received child: %d exit status: %d\n",
+                                            pid, status);
             Delay(2);
         }
     }
+
+    // child process
     int answer = 42 / 0;
     TracePrintf(1, "[trap_math] Child divided by zero answer is: %d\n", answer);
     return 0;
