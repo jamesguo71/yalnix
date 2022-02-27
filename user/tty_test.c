400c86179e0bf662c5a3ee9bd18080732a2444a2
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-27 11:31:36 -0500

    changed to infinite while loop and added a delay to test a user inputting multiple lines before the process can read

--- user/tty_test.c
+++ user/tty_test.c
@@ -3,12 +3,14 @@
 #define MAX_LINE 1024
 
 int main(void) {
-	int num_lines = 2;
-	int terminal  = 0;
+	int terminal = 0;
 	char buf[MAX_LINE];
 	int ret;
-	int test;
-	for (int i = 0; i < num_lines; i++) {
+	int bytes;
+	while (1) {
+
+		// 1. Read a line from the terminal. TtyRead will block until there is input ready.
+		//    The returned line is not null terminated, so be sure to do that yourself.
 		TracePrintf(1, "[tty_test] About to read from terminal: %d\n", terminal);
 		ret = TtyRead(terminal, buf, MAX_LINE);
 		if (ret <= 0 || ret > MAX_LINE) {
@@ -16,8 +18,13 @@ int main(void) {
 			return ret;
 		}
 		buf[ret] = '\0';
-		test = ret;
-		TracePrintf(1, "[tty_test] Read: %d bytes from terminal: %d\n", test, terminal);
+
+		// 2. I noticed that if I try to print ret the value is always 0. I think this is because
+		//    TracePrintf returns immediately but does not execute the action right away. Thus,
+		//    we end up looping back around and the value of ret gets reset because we use it
+		//    for another TtyRead. By saving its previous value in bytes we get the correct print.
+		bytes = ret;
+		TracePrintf(1, "[tty_test] Read: %d bytes from terminal: %d\n", bytes, terminal);
 		TracePrintf(1, "[tty_test] buf: %s\n", buf);
 	}
 	return 0;
