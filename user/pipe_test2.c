9c075e7bd67af28b4d6d19b3c5ddd5b497be3f1d
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-28 16:21:22 -0500

    trying pipe test with very large buffer

--- user/pipe_test2.c
+++ user/pipe_test2.c
@@ -1,32 +1,33 @@
 #include "yuser.h"
 
-#define BUF_LEN  1024
 #define READ_LEN 100
+
+char *write_string = "Write the len bytes starting at buf to the named pipe.\n(As the pipe is a"
+                     "FIFO buffer, these bytes should be appended to the sequence of unread bytes"
+                     "currently in the pipe.)\nReturn as soon as you get the bytes into the buffer"
+                     ".\nIn case of any error, the value ERROR is returned.\nOtherwise, return the"
+                     "number of bytes written.";
 
 int main() {
     // 1. Declare a buffer for reading/writing and initialize a pipe
     int  pipe = PipeInit(&pipe);
-    char buf[BUF_LEN];
-    for (int i = 0; i < BUF_LEN; i++) {
-        buf[i] = 'a';
-    }
-    buf[BUF_LEN - 1] = '\0';
 
     // 2. Fork and have the parent/child take turns reading and writing to one another.
     int pid  = Fork();
     if (pid) {
+        char read_buf[READ_LEN];
         while (1) {
             TracePrintf(1, "[pipe_test] Parent reading from pipe: %d \n", pipe);
-            PipeRead(pipe, (void *) buf, READ_LEN);
-            buf[READ_LEN - 1] = '\0';
-            TracePrintf(1, "[pipe_test] Parent read: %s\n", buf);
+            int read_len = PipeRead(pipe, (void *) read_buf, READ_LEN);
+            read_buf[read_len - 1] = '\0';
+            TracePrintf(1, "[pipe_test] Parent read: %s\n", read_buf);
             Pause();
         }
     } else {
         while (1) {
             TracePrintf(1, "[pipe_test] Child writing to pipe: %d \n", pipe);
-            PipeWrite(pipe, (void *) buf, BUF_LEN);
-            TracePrintf(1, "[pipe_test] Child wrote: %s\n", buf);
+            PipeWrite(pipe, (void *) write_string, strlen(write_string));
+            TracePrintf(1, "[pipe_test] Child wrote: %s\n", write_string);
             Pause();
         }
     }
