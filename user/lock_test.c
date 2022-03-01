e2021d660afaf6614dd9a82960fd01d637e8c9e9
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-02-28 20:17:35 -0500

    adding lock test

---
+++ user/lock_test.c
@@ -0,0 +1,40 @@
+#include "yuser.h"
+
+#define NUM_CHILDREN 10
+
+int main() {
+    // 1. Create a new lock. Exit upon error.
+    int lock = LockInit(&lock);
+    if (lock < 0) {
+        TracePrintf(1, "[lock_test] Error initializing lock. Exiting...\n");
+        return lock;
+    }
+
+    // 2. Spawn a number of child processes, where the child processes delay and then attempt to
+    //    aquire the lock. Once they have it, they print a message, release the lock, and exit.
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        int pid = Fork();
+        if (pid) {
+            TracePrintf(1, "[lock_test] Parent forked child process: %d\n", pid);
+        } else {
+            pid = GetPid();
+            int delay = NUM_CHILDREN * 2 - pid;
+            TracePrintf(1, "[lock_test] Child %d delaying for %d cycles\n", pid, delay);
+            Delay(delay);
+
+            Aquire(lock);
+            TracePrintf(1, "[lock_test] Child %d aquired lock %d\n", pid, lock);
+            Release(lock);
+            return delay;
+        }
+    }
+
+    // 3. After spawning the child processes, the parent should wait for them all to finish.
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        TracePrintf(1, "[lock_test] Parent waiting on any child to finish...\n");
+        int status;
+        int pid = Wait(&status);
+        TracePrintf(1, "[lock_test] Parent received child: %d exit status: %d\n", pid, status);
+    }
+    return 0;
+}
