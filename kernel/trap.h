1d8137082af38f1a54c078a1497e0a094722d6b1
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2022-03-01 13:12:19 -0500

    header comments

--- kernel/trap.h
+++ kernel/trap.h
@@ -14,52 +14,67 @@ int TrapKernel(UserContext *_uctxt);
 
 
 /*!
- * \desc               These requests are ignored (for now)
+ * \desc              This handler gets called every time the clock interrupt fires---the time
+ *                    between clock interrupts is the amount of time (i.e., quantum) that a process
+ *                    gets to run for before being switched out. Before switching out the current
+ *                    process, this handler updates our delay list (i.e., moves processes to the
+ *                    ready list if they hit their delay value) and saves the current process'
+ *                    UserContext in its pcb for future use.
  * 
  * \param[in] _uctxt  The UserContext for the process associated with the TRAP
  * 
- * \return             0 on success, ERROR otherwise.
+ * \return            0 on success, ERROR otherwise.
  */
 int TrapClock(UserContext *_uctxt);
 
 
 /*!
- * \desc               These requests are ignored (for now)
- * 
+ * \desc              This trap handler gets called when a process executes an illegal instruction
+ *                    (how they would do that, I do not know). We handle this by simply printing
+ *                    some debug information on the illegal instruction and exiting the process. 
+ *                     
  * \param[in] _uctxt  The UserContext for the process associated with the TRAP
  * 
- * \return             0 on success, ERROR otherwise.
+ * \return            We do not return on success (because we kill and switch), ERROR otherwise.
  */
 int TrapIllegal(UserContext *_uctxt);
 
 
 /*!
- * \desc               These requests are ignored (for now)
- * 
+ * \desc              This handler gets called when the process executes an illegal memory access,
+ *                    which may be due to (1) violating page protection permissions (2) accessing
+ *                    outside of the processes allowed memory or (3) accessing allowed but unmapped
+ *                    memory (i.e., when the stack grows into an unmapped page). For the first two
+ *                    cases, we simply print debug information and exit the process. For the third,
+ *                    we need to allocate space to allow the stack to grow.
+ *                     
  * \param[in] _uctxt  The UserContext for the process associated with the TRAP
  * 
- * \return             0 on success, ERROR otherwise.
+ * \return            0 on success, otherwise we do not return (because we kill and switch).
  */
 int TrapMemory(UserContext *_uctxt);
 
 
 /*!
- * \desc               These requests are ignored (for now)
+ * \desc              This trap handler gets called when a process executes an illegal math
+ *                    instruction (such as divide by zero). We handle this by simply printing some
+ *                    debug information on the illegal instruction and exiting the process.
  * 
  * \param[in] _uctxt  The UserContext for the process associated with the TRAP
  * 
- * \return             0 on success, ERROR otherwise.
+ * \return            We do not return on success (because we kill and switch), ERROR otherwise.
  */
 int TrapMath(UserContext *_uctxt);
 
 
 /*!
- * \desc               Reads input from the terminal using TtyReceive and buffers the input line
- *                     for subsequent TtyRead syscalls.
+ * \desc              This handler gets called when there is input waiting to be read from one
+ *                    of the terminals. It calls our TTYUpdateReader function, which reads and
+ *                    saves the input into kernel memory for future use by processes.
  * 
  * \param[in] _uctxt  The UserContext for the process associated with the TRAP
  * 
- * \return             0 on success, ERROR otherwise.
+ * \return            0 on success, ERROR otherwise.
  */
 int TrapTTYReceive(UserContext *_uctxt);
 
