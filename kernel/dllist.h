1ed5937e210eb3586006cf2b70e7de4f4b86dcd7
Fei Guo <i.james.guo@gmail.com>
2022-03-06 10:23:25 -0500

    add comments to dllist.h

--- kernel/dllist.h
+++ kernel/dllist.h
@@ -11,9 +11,23 @@ typedef struct {
   dlnode_t *sentinel_node;
 } dllist;
 
+/* Create a new dllist */
 dllist *list_new();
+
+/* Free a dllist and all its nodes*/
 void list_free(dllist *l);
+/*
+ * Appends an id into the dllist
+ * return SUCCESS if this operation was done, otherwise return ERROR
+ */
 int list_append(dllist *l, int id);
+/*
+ * Delete the node with the specified id from the dllist
+ */
 void list_delete_id(dllist *list, int id);
+/*
+ * list_foreach expects its second parameter to be a function pointer that takes the id of each node
+ * as input and returns SUCCESS if the operation on the node's id succeeds
+ */
 void list_foreach(dllist *list, int (*op)(int));
 #endif //YALNIX_FRAMEWORK_YALNIX_KERNEL_DLLIST_H_
